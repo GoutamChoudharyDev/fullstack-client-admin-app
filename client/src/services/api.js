bd11111bf20185d3f8cc3e7f51d2f66f0f2ae3a5
unknown <rc.goutam.choudhary@gmail.com>
2026-01-11 09:22:53 +0530

    url Changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://fullstack-client-admin-app.onrender.com'
+
 const API = axios.create({
     // baseURL: "http://localhost:5000/api",
-    // baseURL: `${import.meta.env.VITE_API_URL}/api`,
-    baseURL: `https://fullstack-client-admin-app.onrender.com/api`,
+    baseURL: `${BASE_URL}/api`,
     withCredentials: true
 });
 
